055f978fbf426d972899c68e7b36b7a3769c4e48
Sifan23 <sifanyg123@gmail.com>
2025-04-03 10:04:43 +0300

    category header and a column for the next section

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,7 +10,20 @@ class HomePage extends StatelessWidget {
         appBar: appBar(),
         backgroundColor: Colors.white,
         body: Column(
-          children: [_searchField()],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _searchField(),
+            SizedBox(height: 40),
+            Column(children: [
+              Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text('Category',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600)))
+            ])
+          ],
         ));
   }
 
